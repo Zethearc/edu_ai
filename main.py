93b2bd71b6f46111edc5548fb0184c851f81a5db
Darío Cabezas <dariosce99@gmail.com>
2023-11-13 09:21:39 -0500

    Last update

--- main.py
+++ main.py
@@ -1,130 +1,203 @@
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-from langchain.llms import HuggingFacePipeline
-from langchain.memory import ConversationBufferWindowMemory
-from langchain.prompts import PromptTemplate
-from langchain.chains import ConversationChain
-import pinecone
-import transformers
-import pinecone
-from sentence_transformers import SentenceTransformer
-import streamlit as st
+import json
+import sys
+import random
+import logging
+import argparse
+import time
+from pathlib import Path
+import queue
+import threading
 
-embed_model_id = 'sentence-transformers/all-MiniLM-L6-v2'
+from llama_cpp import Llama
+import telebot
+from telebot import types
 
-pinecone.init(
-    api_key=st.secrets["PINECONE_API_KEY"],
-    environment=st.secrets["PINECONE_ENVIRONMENT"]
-)
+from db import ChatHistoryDB
 
-index_name = 'edu-ai-indexes'
-index = pinecone.Index(index_name)
+parser = argparse.ArgumentParser(description='LLaMa telegram bot')
+parser.add_argument('token', help='Telegram bot token')
+parser.add_argument('-m', '--model', default="llama-2-7b-chat.ggmlv3.q4_0.bin", help='Path to the LLaMa model')
+parser.add_argument('-t', '--threads', default=4, type=int, help='Number of threads to use')
+parser.add_argument('--max-tokens', default=256, type=int, help='The maximum number of tokens to generate')
+parser.add_argument('--enable-history', action='store_true', help='Simulate memory in a chatbot')
+parser.add_argument('--skip-init-prompt', action='store_true', help='Skip the initial prompt (faster startup)')
+parser.add_argument('--debug', action='store_true', help='Enable debug logging')
 
-model_id = 'meta-llama/Llama-2-7b-chat-hf'
-hf_auth = st.secrets["HF_TOKEN"]
-model_config = transformers.AutoConfig.from_pretrained(
-    model_id,
-    use_auth_token=hf_auth
-)
-model = transformers.AutoModelForCausalLM.from_pretrained(
-    model_id,
-    trust_remote_code=True,
-    config=model_config,
-    device_map='auto',
-    use_auth_token=hf_auth
-)
-tokenizer = transformers.AutoTokenizer.from_pretrained(
-    model_id,
-    use_auth_token=hf_auth
-)
+args = parser.parse_args()
 
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model,
-    tokenizer=tokenizer,
-    device_map="auto",
-    max_length=1000,
-    do_sample=True,
-    top_k=10,
-    num_return_sequences=1,
-    eos_token_id=tokenizer.eos_token_id
-)
+log_level = logging.DEBUG if args.debug else logging.INFO
+log_format = '%(asctime)s [%(levelname)s] %(message)s'
+date_format = '%Y-%m-%d %H:%M:%S'
 
-# Función para buscar en Pinecone y formatear los resultados
-def buscar_en_pinecone(query):
-    index = pinecone.Index('edu-ai-indexes')
-    query_vector = embed_model_id.encode(query).tolist()
-    ejercicios = []
-    material_audiovisual = []
+logging.basicConfig(level=log_level, format=log_format, datefmt=date_format)
+logger = logging.getLogger(__name__)
 
-    responses = index.query(vector=query_vector, top_k=3, include_metadata=True)
 
-    for respuesta in responses["matches"]:
-        metadata = respuesta.get("metadata", {})
-        ejercicios.append(metadata.get("Ejercicios", "No disponible"))
-        material_audiovisual.append(metadata.get("Material_Audiovisual", "No disponible"))
+bot = telebot.TeleBot(args.token)
+model = Path(args.model).resolve()
+seed = random.randint(1, sys.maxsize)
 
-    # Formatear los resultados como un string
-    resultado_formateado = "Ejercicios:\n"
-    resultado_formateado += "\n".join(ejercicios)
-    resultado_formateado += "\n\nMaterial Audiovisual:\n"
-    resultado_formateado += "\n".join(material_audiovisual)
+try:
+    llama = Llama(model_path=str(model), n_ctx=512, seed=seed, n_threads=args.threads, verbose=args.debug)
+except ValueError as e:
+    logger.error("Error while initializing LLaMa: "+str(e))
+    sys.exit(1)
 
-    return resultado_formateado
+historyDb = ChatHistoryDB("chat.db")
+job_queue = queue.Queue()
 
-llm = HuggingFacePipeline(pipeline=pipeline, model_kwargs={'temperature': 0.7})
-
-prompt_template = """<s>[INST] <<SYS>>
-{{Eres EDUAI, un chatbot desarrollado por Yachay Tech para ayudar a estudiantes de matemáticas. Sigue estas reglas:
-    Responde como asistente.
-    Sé amable, conciso y rápido.
-    No saludes en cada respuesta, solo responde la pregunta.
-    Anima al estudiante a seguir aprendiendo.
-    Responde en español siempre.
-    Utiliza el formato markdown y viñetas.
-    Usa ejercicios y material audiovisual para complementar tus respuestas cuando estén disponibles.}}<<SYS>>
-###
-
-Previous Conversation:
+init_prompt = '''Tu nombre es EDUAI, un asistente virtual desarrollado por las universidades Yachay Tech y UIDE en Ecuador. Tu propósito es brindar ayuda a estudiantes en matemáticas, tanto de colegios como de universidades.
+Actúas como un asistente, no como un usuario. Responde desde tu función específica.
+Mantén respuestas amables, concisas y rápidas para una mejor experiencia.
+Evita saludar en cada respuesta; responde directamente a la pregunta del usuario.
+Anima a los estudiantes a seguir aprendiendo de manera constante.
+Responde siempre en español para mantener la coherencia.
+Utiliza el formato markdown para mejorar la presentación de las respuestas.
+Incorpora emojis para enriquecer la experiencia del usuario.
+Mantén un tono positivo y motivador en tus interacciones, incentivando el interés y la pasión por las matemáticas.
 '''
-{history}
-'''
+q_prompt = "### Instruction:"
+a_prompt = "### Response:"
 
-{{{input}}}[/INST]
 
-"""
+def process_job(job):
+    def generate_text(user_prompt, max_tokens=args.max_tokens, stream=False, custom_prompt=False, chat_id=None,
+                      history=args.enable_history):
+        if args.skip_init_prompt:
+            prompt = f"{q_prompt} {user_prompt}\n{a_prompt}"
+        else:
+            prompt = f"{init_prompt}\n{q_prompt} {user_prompt}\n{a_prompt}"
+        if custom_prompt:
+            prompt = user_prompt
+        if history and chat_id:
+            prompt = get_last_messages(chat_id) + prompt
 
-memory = ConversationBufferWindowMemory(k=5)
+        logger.debug(f"Generation for: {prompt}")
 
-prompt = PromptTemplate(template=prompt_template, input_variables=['input', 'history'])
-chain = ConversationChain(
-    llm=llm,
-    prompt=prompt,
-    memory=memory
-)
+        if stream and max_tokens > 2048:
+            logger.warning("This is likely to exceed 4096 characters, which would not fit into one stream message")
 
-st.set_page_config(page_title="EDUAI-CHAT")
+        return llama.create_completion(prompt, max_tokens=max_tokens, top_k=100, top_p=0.95, temperature=0.7,
+                                           stream=stream, stop=["<|endoftext|>", "###", a_prompt, q_prompt])
 
-st.title("EDUAI-CHAT")
+    user_prompt = job[0]
+    chat_id = job[1]
+    msg = job[2]
+    custom_prompt = job[3]
 
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+    try:
+        bot.edit_message_text(chat_id=msg.chat.id, text="Started to generate text for you...",
+                              message_id=msg.message_id)
+        logger.info(f"Generating text for user {msg.chat.username}")
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+        if custom_prompt:
+            json_obj = generate_text(user_prompt, chat_id=chat_id, stream=False,
+                                     custom_prompt=True)
+        else:
+            json_obj = generate_text(user_prompt, chat_id=chat_id, stream=False)
+        output = json_obj['choices'][0]["text"]
 
-# React to user input
-if prompt := st.chat_input("What is up?"):
-    # Display user message in chat message container
-    st.chat_message("user").markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+        logger.debug(json.dumps(json_obj, indent=2))
+        text_to_user = output
 
-    response = chain.run(user_prompt)
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        st.markdown(response)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        send_by_chunks(msg, text_to_user)
+        logger.info(f"Sent to user {msg.chat.username}: {text_to_user}")
+
+        if args.enable_history:
+            historyDb.insert_message(chat_id, user_prompt, output)
+        bot.delete_message(msg.chat.id, msg.message_id)  # delete 'please wait a moment'
+    except OSError as e:
+        bot.reply_to(msg, f"OSError: {e}")
+    except Exception as e:
+        bot.reply_to(msg, f"Error: {e}")
+
+
+def process_queue():
+    while True:
+        try:
+            job = job_queue.get()
+            start_time = time.time()
+            process_job(job)
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            logger.info(f"Job processed in {elapsed_time:02f} seconds")
+            job_queue.task_done()
+        except Exception as e:
+            logger.error(f"Error: {e}")
+
+
+def send_by_chunks(message, text, **kwargs):
+    if len(text) < 5:
+        logger.error("Message is empty or too short")
+    if len(text) <= 4096:
+        bot.send_message(message.chat.id, text, **kwargs)
+    else:
+        chunks = []
+        while len(text) > 0:
+            chunk = text[:4096]
+            chunks.append(chunk)
+            text = text[4096:]
+        for chunk in chunks:
+            bot.reply_to(message, chunk, **kwargs)
+
+
+def get_last_messages(chat_id):
+    messages = historyDb.get_chat_messages(chat_id)
+    history = ""
+    for user_prompt, answer in messages:
+        history += f"{q_prompt} {user_prompt}\n {a_prompt} {answer}\n"
+    return history
+
+
+@bot.message_handler(commands=['history'])
+def history_command(message):
+    keyboard = types.InlineKeyboardMarkup()
+    button = types.InlineKeyboardButton(text="Delete history", callback_data="remove_history")
+    keyboard.add(button)
+    history = get_last_messages(message.chat.id)
+    if history == "":
+        history = "You have no history with the bot"
+    bot.send_message(message.chat.id, text=history, reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'remove_history')
+def send_message_callback(call):
+    historyDb.delete_all_history(call.message.chat.id)
+    bot.send_message(call.message.chat.id, "Chat history successfully forgotten")
+
+
+@bot.message_handler(commands=['start', 'help'])
+def start_command(message):
+    bot.reply_to(message, "Hello. This is chatGPT bot based on LLaMa.\nUsage:"
+                          "\n\n<text> - Q&A mode.\n"
+                          "/raw <prompt> - Use your own prompt.\n"
+                          "/history - show history and delete it\n\n"
+                          f"Current model: {model.name}")
+
+
+@bot.message_handler(commands=['raw'])
+def raw_command(message):
+    user_prompt = message.text.replace("/raw ", '', 1)
+
+    msg = bot.reply_to(message, f"Please wait a moment. Current queue: {job_queue.qsize()}")
+    bot.send_chat_action(chat_id=message.chat.id, action='typing')
+    job_queue.put((user_prompt, message.chat.id, msg, True))
+
+
+@bot.message_handler(func=lambda message: True)
+def main(message):
+    if message.text.startswith("/"):
+        bot.reply_to(message, "Wrong command")
+        return
+    user_prompt = message.text
+    msg = bot.reply_to(message, f"Please wait a moment. Current queue: {job_queue.qsize()}")
+    bot.send_chat_action(chat_id=message.chat.id, action='typing')
+    job_queue.put((user_prompt, message.chat.id, msg, False))
+    logger.info("Added a new task from user: %s (%s), text: %s",  message.chat.username,  message.chat.id, user_prompt)
+
+
+t = threading.Thread(target=process_queue)
+t.daemon = True
+t.start()
+
+bot.infinity_polling()
